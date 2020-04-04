8e4fd04e9ac636ba21c6d6f57cc014e00b44b09b
Kwinten <kwinten.yc.li@gmail.com>
2020-04-04 17:57:04 +0800

    Draft system to publish blog post when it is ready

--- backend/api/article/controllers/article.js
+++ backend/api/article/controllers/article.js
@@ -1,6 +1,25 @@
 const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
 
 module.exports = {
+  async find(ctx) {
+    let entities;
+
+    ctx.query = {
+      ...ctx.query,
+      status: "published",
+    };
+
+    if (ctx.query._q) {
+      entities = await strapi.services.article.search(ctx.query);
+    } else {
+      entities = await strapi.services.article.find(ctx.query);
+    }
+
+    return entities.map((entity) =>
+      sanitizeEntity(entity, { model: strapi.models.article })
+    );
+  },
+
   /**
    * Create a record.
    *
