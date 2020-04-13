07fa2209f395d55b90c0bc19a12086e8ef2076ad
Kwinten <kwinten.yc.li@gmail.com>
2020-04-13 16:31:34 +0800

    changed the way creating seed data to cater non-sqlite type database

---
+++ backend/seed/seed.js
@@ -0,0 +1,33 @@
+module.exports = {
+  articles: [
+    {
+      mimeType: "image/jpeg",
+      imageFileName: "blog-header-network.jpg",
+      status: "published",
+      title: "Thanks for giving this Starter a try!",
+      content: `# Thanks
+    We hope that this starter will make you want to discover Strapi in more details.
+    
+    ## Features
+    
+    - 2 Content types: Article, Category
+    - Permissions set to 'true' for article and category
+    - 2 Created Articles
+    - 3 Created categories
+    - Responsive design using UIkit
+    
+    ## Pages
+    
+    - "/" display every articles
+    - "/article/:id" display one article
+    - "/category/:id" display articles depending on the category`,
+    },
+    {
+      mimeType: "image/jpeg",
+      imageFileName: "blog-header-balloon.jpg",
+      status: "published",
+      title: "Enjoy!",
+      content: "Have fun!",
+    },
+  ],
+};
