a6bae49d5be398781e5782f36d6506f3356b23ba
Kwinten <kwinten.yc.li@gmail.com>
2020-04-14 18:11:40 +0800

    default upload to cloudinary

--- backend/config/functions/bootstrap.js
+++ backend/config/functions/bootstrap.js
@@ -86,10 +86,36 @@ const createSeedData = async () => {
   await Promise.all(articlesPromises);
 };
 
+const setDefaultFileUploader = async () => {
+  if (strapi.config.environment !== "production") {
+    return;
+  }
+  const pluginStore = strapi.store({
+    environment: strapi.config.environment,
+    type: "plugin",
+    name: "upload",
+  });
+  const config = await pluginStore.get({ key: "provider" });
+  await pluginStore.set({
+    key: "provider",
+    value: {
+      ...config,
+      ...{
+        provider: "cloudinary",
+        name: "Cloudinary",
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+        api_key: process.env.CLOUDINARY_API_KEY,
+        api_secret: process.env.CLOUDINARY_API_SECRET,
+      },
+    },
+  });
+};
+
 module.exports = async () => {
   const shouldInit = await isFirstRun();
   if (shouldInit) {
     await setDefaultPermissions();
+    await setDefaultFileUploader();
     await createSeedData();
   }
 };
